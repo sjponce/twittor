bf15457a874175376ab66c7a912ab02dba38e8fe
santiago <santiago.ponce@kunan.com.ar>
2019-12-23 16:50:13 -0300

    red

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v4';
-const DYNAMIC_CACHE = 'dynamic-v2';
+const STATIC_CACHE = 'static-v5';
+const DYNAMIC_CACHE = 'dynamic-v3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
